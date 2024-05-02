a99af0af5ec20616ed2ee3e27e6d04033458a517
Denis <me@hotdog.com>
2024-05-02 17:05:50 +0300

    Finished chapter 18

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -12,3 +12,10 @@ def topics(request):
     topics = Topic.objects.order_by("date_added")
     context = {"topics": topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a topic and all its entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by("date_added")
+    context = {"topic": topic_id, "entries": entries}
+    return render(request, 'learning_logs/topic.html', context)
